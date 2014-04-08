47b0636238dd648cd17c87efdebc3375a621825e
ExTheSea <spammailmr@web.de>
2014-04-08 21:07:13 +0200

    Added Comments

--- src/com/server/main/RequestThread.java
+++ src/com/server/main/RequestThread.java
@@ -17,16 +17,16 @@ public class RequestThread extends Thread {
 	public RequestThread(Socket clientsocket) {
 		
 		this.clientsocket = clientsocket;
-		this.start();
+		this.start(); //Starten des Threads um die Request enthalten in der clientsocket Variable zu bearbeiten
 	}
 	
 	@Override
 	public void run() {
 		try {
-			InputStream str = clientsocket.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(str));
-			String reqstr = br.readLine();
-			String getstr = reqstr.substring(reqstr.indexOf("GET")+3, reqstr.lastIndexOf("HTTP")).trim();
+			InputStream str = clientsocket.getInputStream(); //Http Request Header und Body aus socket als InputStream
+			BufferedReader br = new BufferedReader(new InputStreamReader(str)); //BufferedReader um InputStream zu lesen
+			String reqstr = br.readLine(); //Lesen der ersten Zeile, in der der GET pfad liegt, wie z.B. "GET /somefile.txt HTTP/1.1"
+			String getstr = reqstr.substring(reqstr.indexOf("GET")+3, reqstr.lastIndexOf("HTTP")).trim(); //Heraus lesen des Request-Pfads
 
 			handlereq(getstr, clientsocket.getOutputStream());
 
@@ -36,46 +36,55 @@ public class RequestThread extends Thread {
 		}
 	}	
 	
+	
+	/**
+	 * Genaue Behandlung des Requests.
+	 * @param requeststr String, der auf ein Verzeichnis oder Datei zielt, z.B. "/somefolder/somehtmlfile.html"
+	 * @param os OutputStream, in den die Response geschrieben wird
+	 * @throws IOException
+	 */
 	public void handlereq(String requeststr, OutputStream os) throws IOException{
-		File f = new File(StartServer.filelocation+requeststr);
+		File f = new File(StartServer.filelocation+requeststr); //Instanz von File, die auf den tatsächlichen Ordner/Datei auf dem System innerhalb des files Ordner zeigt
 		if(f.exists()){
-			if(f.isDirectory()){
-				File[] farr = f.listFiles();
-				StringBuilder htmlstringbuilder = new StringBuilder();
-				htmlstringbuilder.append("<html><body>");
+			if(f.isDirectory()){ //Wenn es sich um ein Verzeichnis handelt
+				File[] farr = f.listFiles(); //Auflisten aller Dateien und Ordner innerhalb des Verzeichnis
+				StringBuilder htmlstringbuilder = new StringBuilder(); //Zum Aufbau des Htmls zur Anzeige der Dateien und Ordner
+				htmlstringbuilder.append("<html><body>"); 
 				
-				for (int i = 0; i < farr.length; i++) {
+				for (int i = 0; i < farr.length; i++) { //Einen Link pro Datei/Ordner zu dem String hinzufügen
 					File file = farr[i];
 					htmlstringbuilder.append("<a href="+file.getAbsolutePath().replace(StartServer.filelocation+"\\", "").replace("\\", "/")+">"+file.getName()+"</a><br>");
 				}
 				
 				htmlstringbuilder.append("</body></html>");
 				
-				os.write("HTTP/1.1 200 OK \r\n".getBytes());
+				os.write("HTTP/1.1 200 OK \r\n".getBytes()); //Standard Header für HTml Dateien um Probleme in manchen Browsern zu umgehen
 				os.write("Content-Type:text/html \r\n".getBytes());
-				os.write("\r\n".getBytes());
-				os.write(htmlstringbuilder.toString().getBytes());
-				os.flush();
-				os.close();
-			}else if(f.isFile()){
-				FileInputStream fs = new FileInputStream(f);
+				os.write("\r\n".getBytes()); // \r\n fügt einen Absatz ein. Nach dem Header muss ein doppelter Absatz eingefügt werden um den Content abzutrennen
+				os.write(htmlstringbuilder.toString().getBytes()); //Schreiben des Contents zum Outputstream
+				os.flush(); 
+				os.close(); //Flush + Close um Response zum Client zu schicken
+			}else if(f.isFile()){ //Wenn es sich um eine File handelt
+				FileInputStream fs = new FileInputStream(f); 
 
 			    final byte[] buffer = new byte[1024];
 			    int count = 0;
-			    String conttype = URLConnection.guessContentTypeFromName(f.getName());
-			    os.write("HTTP/1.1 200 OK \r\n".getBytes());
+			    
+			    String conttype = URLConnection.guessContentTypeFromName(f.getName()); //MIME Type der Datei herrausfinden
+			    
+			    os.write("HTTP/1.1 200 OK \r\n".getBytes()); //Header hinzufügen
 				os.write(("Content-Type:"+conttype+" \r\n").getBytes());
 				os.write("\r\n".getBytes());
 				
 			    while ((count = fs.read(buffer)) >= 0) {
-			        os.write(buffer, 0, count);
+			        os.write(buffer, 0, count); //Schreiben der Datei zum Outputstream
 			    }
 			    os.flush();
 			    fs.close();
 			    os.close();
 			}
-		}else{
-			os.write("HTTP/1.1 404 Not Found \r\n".getBytes());
+		}else{ //Wenn Datei nicht existiert
+			os.write("HTTP/1.1 404 Not Found \r\n".getBytes()); //Zurückgeben eines Standard 404 Not Found Headers und eine Html Seite. 
 			os.write("Content-Type:text/html \r\n".getBytes());
 			os.write("\r\n".getBytes());
 			os.write(("<!DOCTYPE html><html lang=en>  <meta charset=utf-8>  <meta name=viewport content='initial-scale=1, minimum-scale=1, width=device-width'>  <title>Error 404 (Not Found)!!1</title>  <style>    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}  </style>  <a href=/><span id=logo aria-label=Google></span></a>  <p><b>404.</b> <ins>That is an error.</ins>  <p>The requested URL "+requeststr+" was not found on this server.  <ins>That is all we know.</ins>").getBytes());
--- src/com/server/main/StartServer.java
+++ src/com/server/main/StartServer.java
@@ -18,18 +18,18 @@ public class StartServer {
 	static String filelocation = "";
 	
 	public static void main(String[] args) {
-		if(filelocation.isEmpty()){
+		if(filelocation.isEmpty()){ // Wenn noch nicht gesetzt wird der Pfad zum "files" Ordner der dort liegen sollte, wo das Programm ausgeführt wird
 			File f = new File( RequestThread.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 			f = f.getParentFile();
 			f = new File(f.getAbsolutePath()+"/files");
 			filelocation = f.getAbsolutePath();
 		}
-		new StartServer();
+		new StartServer(); //Erstellen einer Instanz dieser Klasse um nicht-statische Variablen/Methoden zu nutzen
 	}
 	
 	public StartServer() {
 
-		final JFrame frame = new JFrame("Server");
+		final JFrame frame = new JFrame("Server"); //Erstellen eines simplen Fensters (mit Java swing) mit einem Button um den Server zu schließen
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
  
         JButton btn = new JButton("Close Server");
@@ -39,33 +39,37 @@ public class StartServer {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					servsocket.close();
-					frame.dispose();
+					servsocket.close(); //Schließen des sockets
+					frame.dispose(); //Schließen des Fensters
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		});
  
-        frame.setSize(200, 200);
+        frame.setSize(200, 200); //Fenster (Frame) anzeigen
         frame.pack();
         frame.setVisible(true);
         
 		try {
-			servsocket = new ServerSocket(8080);
-			checkrequest();
+			servsocket = new ServerSocket(8080); //Initieren des ServerSockets auf Port 8080
+			checkrequest(); 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 	}
 	
+	/**
+	 * While-loop, der bei jeder rein kommenden Request einen RequestThread startet um diesen zu bearbeiten
+	 * @throws IOException
+	 */
 	public void checkrequest() throws IOException{
-		while (!servsocket.isClosed()) {
+		while (!servsocket.isClosed()) { //Loop stoppen when ServerSocket geschlossen
 			try{
 			Socket clientsocket = servsocket.accept();
 			new RequestThread(clientsocket);
-			}catch(SocketException e){};
+			}catch(SocketException e){}; //Exception wird geworfen, wenn socket während socket.accept() geschlossen wird
 		}
 	}
 	
